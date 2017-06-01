86184050f2815e5ab1ba6ea036967e289ec642fb
Miroslav Oleinik <ollmirik@gmail.com>
2017-06-01 11:49:50 +0300

    Update main.cpp
    Add menu.

--- HomeWork/HomeWork 31.05.2017/Task 2. Stack/main.cpp
+++ HomeWork/HomeWork 31.05.2017/Task 2. Stack/main.cpp
@@ -1,13 +1,46 @@
-﻿#include <iostream>
+#include <iostream>
+#include <locale.h>
 #include "Stack.h"
 
+enum Menu
+{
+	Push = 1,
+	Pop,
+	Top,
+	Clear
+};
 
 int main()
 {
+	setlocale(LC_ALL, "Rus");
 	Stack s1;
-	std::cout << "Entre number: ";
+	int menu;
 	int element;
-	std::cin >> element;
-	s1 << element;
-	std::cout << s1.Top();
+	while (0 < 1)
+	{
+		std::cout << "\tMenu\n1. Add new number to stack.\n2. Took number from stack.\n3. Show last number in stack.\n4. Clear all stack.\nEntre: ";
+		std::cin >> menu;
+		switch (menu)
+		{
+		case Push:
+			std::cout << "Entre your number: ";
+			std::cin >> element;
+			s1 << element;
+			break;
+		case Pop:
+			s1 >> element;
+			if (element == NULL)
+			{
+				continue;
+			}
+			std::cout << element << "Element was took from stack.\n";
+			break;
+		case Top:
+			break;
+		case Clear:
+			break;
+		default:
+			break;
+		}
+	}
 }
