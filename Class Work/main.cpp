e08948c1669ec8a9967d60041dce6991f0df8e36
Miroslav Oleinik <ollmirik@gmail.com>
2017-06-22 11:09:50 +0300

    Add files via upload

--- Class Work/main.cpp
+++ Class Work/main.cpp
@@ -1,13 +1,23 @@
+#include "DynArray.h"
 #include <iostream>
-#include "List.h"
 
 int main()
 {
-	List list;
+	DynArray<int> d = { 200, 300, 100 };
 
-	for (Iterator it = list.begin(); it != list.end(); ++it)
+	DynArray<int> d1 = d * 5;
+	std::cout << d1.GetCount() << "\n";
+
+	/*int arr[] = { 5, 6, 7, 8 };
+	for (int el : arr)
 	{
+	std::cout << el << " ";
+	} */
 
-		std::cout << *it << " ";
-	}
+	DynArray<int> const d2(d1);
+	std::cout << d2[0] << "\n";
+	d1[0] = -5;
+	std::cout << d1[0] << "\n";
+
+	return 0;
 }
