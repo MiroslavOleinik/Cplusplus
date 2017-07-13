932ab6eccdbc037567139aaa3508f2a9511255fd
Miroslav Oleinik <ollmirik@gmail.com>
2017-07-13 22:25:18 +0300

    Update PhoneBook.h

--- ExamTask/PhoneBook.h
+++ ExamTask/PhoneBook.h
@@ -4,44 +4,69 @@
 #include <list>
 #include <iostream>
 
-class IPhoneBook
-{
-public:
-	virtual ~IPhoneBook() = default;
-
-	virtual std::string GetName() = 0;
-	virtual std::string GetNumber() = 0;
-};
-
-class GetPhoneMemberName : public IPhoneBook
+class PhoneBook
 {
 private:
 	std::string m_name;
-	std::string m_phoneNumber;
+	int m_phoneNumbers[10];
+	int m_count = 0;
 public:
-	GetPhoneMemberName(std::string name, std::string phoneNumber) :
-		m_name(name),
-		m_phoneNumber(phoneNumber)
-	{
-	}
+	PhoneBook(std::string name, int phoneNumber);
 
-	std::string GetName()
-	{
-		return m_name;
-	}
-	
-	std::string GetNumber()
-	{
-		return m_phoneNumber;
-	}
-
+	int GetCount();
+	void AddNewNumber(int phoneNumber);
+	void RemoveNumber(int count);
+	void PrintMembersPhoneNumbers();
 };
 
-class NewPhoneMemberNumber : public GetPhoneMemberName
+PhoneBook::PhoneBook(std::string name, int phoneNumber)
+{
+	m_name = name;
+	m_phoneNumbers[0] = phoneNumber;
+	m_count += 1;
+}
+
+int PhoneBook::GetCount()
+{
+	return m_count;
+}
+
+void PhoneBook::AddNewNumber(int phoneNumber)
+{
+	m_phoneNumbers[m_count] = phoneNumber;
+	m_count += 1;
+}
+
+void PhoneBook::RemoveNumber(int count)
+{
+	m_phoneNumbers[count] = NULL;
+	for (int i = count; i < m_count; i++)
+	{
+		m_phoneNumbers[i] = m_phoneNumbers[i + 1];
+	}
+	m_phoneNumbers[m_count] = NULL;
+	m_count -= 1;
+}
+
+void PhoneBook::PrintMembersPhoneNumbers()
+{
+	std::cout << m_name << "\'s phone numbers:\n";
+	for (int i = 0; i < m_count; i++)
+	{
+		std::cout << i+1 << ": " << m_phoneNumbers[i] << std::endl;
+	}
+}
+
+class Favorites
 {
 public:
-	NewPhoneMemberNumber(std::string name, std::string phoneNumber) : GetPhoneMemberName(name, phoneNumber)
-	{
-	}
+	Favorites(PhoneBook* favoriteMember);
+
+private:
+	std::list<PhoneBook*> m_favoriteMember;
 };
 
+Favorites::Favorites(PhoneBook* favoriteMember)
+{
+	m_favoriteMember.push_back(favoriteMember);
+}
